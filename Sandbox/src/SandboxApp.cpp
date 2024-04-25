dfb784d0b75b1bb84a6691f58e1c6ca4981e6158
William <williamenglish1220@gmail.com>
2024-04-25 13:23:44 -0400

    Added Example Layer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,10 +2,26 @@
 
 #include "CatEngine.h"
 
+class ExampleLayer : public CatEngine::Layer {
+public:
+	ExampleLayer()
+		:Layer("Example") {}
+	
+	void OnUpdate() override 
+	{
+		DEBUG_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(CatEngine::Events& event) override 
+	{
+		DEBUG_TRACE("{0}", event.ToString());
+	}
+};
+
 class Sandbox : public CatEngine::Application {
 public:
 	Sandbox() {
-
+		PushLayer(new ExampleLayer());
 	}
 	~Sandbox() {
 
