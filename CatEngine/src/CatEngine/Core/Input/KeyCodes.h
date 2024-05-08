5329d9bba78729983d40594573f3869080abcd79
William <williamenglish1220@gmail.com>
2024-05-08 13:56:27 -0400

    Added Key and Mouse Codes and move into Input Folder

---
+++ CatEngine/src/CatEngine/Core/Input/KeyCodes.h
@@ -0,0 +1,137 @@
+#pragma once
+
+// From glfw3.h
+#define CE_SPACE              32
+#define CE_APOSTROPHE         39  /* ' */
+#define CE_COMMA              44  /* , */
+#define CE_MINUS              45  /* - */
+#define CE_PERIOD             46  /* . */
+#define CE_SLASH              47  /* / */
+#define CE_0                  48
+#define CE_1                  49
+#define CE_2                  50
+#define CE_3                  51
+#define CE_4                  52
+#define CE_5                  53
+#define CE_6                  54
+#define CE_7                  55
+#define CE_8                  56
+#define CE_9                  57
+#define CE_SEMICOLON          59  /* ; */
+#define CE_EQUAL              61  /* = */
+#define CE_A                  65
+#define CE_B                  66
+#define CE_C                  67
+#define CE_D                  68
+#define CE_E                  69
+#define CE_F                  70
+#define CE_G                  71
+#define CE_H                  72
+#define CE_I                  73
+#define CE_J                  74
+#define CE_K                  75
+#define CE_L                  76
+#define CE_M                  77
+#define CE_N                  78
+#define CE_O                  79
+#define CE_P                  80
+#define CE_Q                  81
+#define CE_R                  82
+#define CE_S                  83
+#define CE_T                  84
+#define CE_U                  85
+#define CE_V                  86
+#define CE_W                  87
+#define CE_X                  88
+#define CE_Y                  89
+#define CE_Z                  90
+#define CE_LEFT_BRACKET       91  /* [ */
+#define CE_BACKSLASH          92  /* \ */
+#define CE_RIGHT_BRACKET      93  /* ] */
+#define CE_GRAVE_ACCENT       96  /* ` */
+#define CE_WORLD_1            161 /* non-US #1 */
+#define CE_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define CE_ESCAPE             256
+#define CE_ENTER              257
+#define CE_TAB                258
+#define CE_BACKSPACE          259
+#define CE_INSERT             260
+#define CE_DELETE             261
+#define CE_RIGHT              262
+#define CE_LEFT               263
+#define CE_DOWN               264
+#define CE_UP                 265
+#define CE_PAGE_UP            266
+#define CE_PAGE_DOWN          267
+#define CE_HOME               268
+#define CE_END                269
+#define CE_CAPS_LOCK          280
+#define CE_SCROLL_LOCK        281
+#define CE_NUM_LOCK           282
+#define CE_PRINT_SCREEN       283
+#define CE_PAUSE              284
+#define CE_F1                 290
+#define CE_F2                 291
+#define CE_F3                 292
+#define CE_F4                 293
+#define CE_F5                 294
+#define CE_F6                 295
+#define CE_F7                 296
+#define CE_F8                 297
+#define CE_F9                 298
+#define CE_F10                299
+#define CE_F11                300
+#define CE_F12                301
+#define CE_F13                302
+#define CE_F14                303
+#define CE_F15                304
+#define CE_F16                305
+#define CE_F17                306
+#define CE_F18                307
+#define CE_F19                308
+#define CE_F20                309
+#define CE_F21                310
+#define CE_F22                311
+#define CE_F23                312
+#define CE_F24                313
+#define CE_F25                314
+#define CE_KP_0               320
+#define CE_KP_1               321
+#define CE_KP_2               322
+#define CE_KP_3               323
+#define CE_KP_4               324
+#define CE_KP_5               325
+#define CE_KP_6               326
+#define CE_KP_7               327
+#define CE_KP_8               328
+#define CE_KP_9               329
+#define CE_KP_DECIMAL         330
+#define CE_KP_DIVIDE          331
+#define CE_KP_MULTIPLY        332
+#define CE_KP_SUBTRACT        333
+#define CE_KP_ADD             334
+#define CE_KP_ENTER           335
+#define CE_KP_EQUAL           336
+#define CE_LEFT_SHIFT         340
+#define CE_LEFT_CONTROL       341
+#define CE_LEFT_ALT           342
+#define CE_LEFT_SUPER         343
+#define CE_RIGHT_SHIFT        344
+#define CE_RIGHT_CONTROL      345
+#define CE_RIGHT_ALT          346
+#define CE_RIGHT_SUPER        347
+#define CE_MENU               348
+
+#define CE_MOD_SHIFT           0x0001
+ 
+#define CE_MOD_CONTROL         0x0002
+
+#define CE_MOD_ALT             0x0004
+
+#define CE_MOD_SUPER           0x0008
+
+#define CE_MOD_CAPS_LOCK       0x0010
+
+#define CE_MOD_NUM_LOCK        0x0020
