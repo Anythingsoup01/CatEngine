97b58116f184e0d9774d19deae2493e1f73293d6
William <williamenglish1220@gmail.com>
2024-09-03 09:17:15 -0400

    Added Uniform Buffer

---
+++ CatEngine/src/CatEngine/Renderer/UniformBuffer.cpp
@@ -0,0 +1,19 @@
+#include "cepch.h"
+#include "UniformBuffer.h"
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLUniformBuffer.h"
+
+namespace CatEngine
+{
+	Ref<UniformBuffer> UniformBuffer::Create(uint32_t size, uint32_t binding)
+	{
+		switch (Renderer::GetCurrentAPI())
+		{
+		case RendererAPI::API::None: API_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
+		case RendererAPI::API::OpenGL: return CreateRef<OpenGLUniformBuffer>(size, binding);
+		}
+
+		API_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
