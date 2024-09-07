c727f15485f040cffe06fa46885c1e1130aa9e84
William <williamenglish1220@gmail.com>
2024-09-07 16:24:44 -0400

    Added AddOrReplaceComponent & some component getters

--- CatEngine/src/CatEngine/Scene/Entity.h
+++ CatEngine/src/CatEngine/Scene/Entity.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Scene.h"
+#include "Components.h"
 
 #include <entt.hpp>
 
@@ -24,6 +25,14 @@ namespace CatEngine
 			return component;
 		}
 
+		template<typename T, typename... Args>
+		T& AddOrReplaceComponent(Args&&... args)
+		{
+			T& component = m_Scene->m_Registry.emplace_or_replace<T>(m_EntityHandle, std::forward<Args>(args)...);
+			m_Scene->OnComponentAdded<T>(*this, component);
+			return component;
+		}
+		
 		template<typename T>
 		T& GetComponent()
 		{
@@ -39,9 +48,7 @@ namespace CatEngine
 			{
 				return GetComponent<T>();
 			}
-			
 			return AddComponent<T>();
-			
 		}
 
 		template<typename T>
@@ -68,6 +75,9 @@ namespace CatEngine
 		operator bool() const { return m_EntityHandle != entt::null; }
 		operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 		operator entt::entity() const { return m_EntityHandle; }
+
+		UUID GetUUID() { return GetComponent<IDComponent>().ID; }
+		const std::string& GetName() { return GetComponent<NameComponent>().Name; }
 
 		bool operator==(const Entity& other) const { return m_EntityHandle == other.m_EntityHandle && m_Scene == other.m_Scene; }
 		bool operator!=(const Entity& other) const { return !(*this == other); }
