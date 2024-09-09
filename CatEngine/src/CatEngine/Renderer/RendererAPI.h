0b07e31665678dad27705bd6e42352a0ed4e4d39
William <williamenglish1220@gmail.com>
2024-09-09 12:31:20 -0400

    Added Set Line Thickness to RenderAPI

--- CatEngine/src/CatEngine/Renderer/RendererAPI.h
+++ CatEngine/src/CatEngine/Renderer/RendererAPI.h
@@ -22,6 +22,7 @@ namespace CatEngine {
 
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount) = 0;
 		virtual void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount) = 0;
+		virtual void SetLineThickness(float thickness) = 0;
 
 		static API GetAPI() { return s_API; }
 		static Scope<RendererAPI> Create();
