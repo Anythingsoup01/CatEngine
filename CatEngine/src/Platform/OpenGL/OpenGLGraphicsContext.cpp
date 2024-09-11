4253265404d9eb5ae2907ab143249c8537bd225d
William <williamenglish1220@gmail.com>
2024-09-11 13:37:37 -0400

    Displaying opengl information

--- CatEngine/src/Platform/OpenGL/OpenGLGraphicsContext.cpp
+++ CatEngine/src/Platform/OpenGL/OpenGLGraphicsContext.cpp
@@ -17,6 +17,20 @@ namespace CatEngine {
 		glfwMakeContextCurrent(m_Window);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		API_ASSERT(status, "Failed to load GLAD");
+
+		CE_API_INFO("OpenGL Info:");
+		CE_API_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		CE_API_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		CE_API_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+#ifdef CE_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		API_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "CatEngine requires at least OpenGL version 4.5!");
+#endif
 	}
 
 	void OpenGLGraphicsContext::SwapBuffers()
