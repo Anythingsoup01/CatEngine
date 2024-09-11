d840d98c93ddf600d532151891c69e35eb00eb98
William <williamenglish1220@gmail.com>
2024-09-11 13:26:11 -0400

    Switched opengl index buffer to use Arrays instead of element arrays

--- CatEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ CatEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -42,8 +42,8 @@ namespace CatEngine {
 	{
 		CE_PROFILE_FUNCTION();
 		GLCall(glCreateBuffers(1, &m_RendererID));
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
-		GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+		GLCall(glBufferData(GL_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW));
 	}
 	OpenGLIndexBuffer::~OpenGLIndexBuffer()
 	{
