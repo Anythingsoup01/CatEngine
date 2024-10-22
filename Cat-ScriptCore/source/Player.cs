24fedece7779c6dd2432c02b9d33780671e38a7f
William <williamenglish1220@gmail.com>
2024-10-22 09:08:53 -0400

    Added basic player script

---
+++ Cat-ScriptCore/source/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CatEngine;
+
+public class Player : MeownoBehaviour
+{
+	public float m_Speed = 5f;
+	public Vector3 m_Velocity;
+
+	private void Start()
+	{
+		Console.WriteLine("Hello World!");
+	}
+
+	private void Update(float ts)
+	{
+		// Basic movement
+		//Vector3 position = Position;
+		//position.X += m_Speed * ts;
+		//Position = position;
+
+		// Intermediate movement
+		m_Velocity = Vector3.Zero;
+
+		if (Input.IsKeyDown(KeyCode.W))
+			m_Velocity.Y = 1f;
+		if (Input.IsKeyDown(KeyCode.S))
+			m_Velocity.Y = -1f;
+		if (Input.IsKeyDown(KeyCode.D))
+			m_Velocity.X = 1f;
+		if (Input.IsKeyDown(KeyCode.A))
+			m_Velocity.X = -1f;
+
+		Vector3 position = Transform.Position;
+
+		position += m_Velocity * m_Speed * ts;
+
+		
+		
+		Transform.Position = position;
+	}
+
+}
+
