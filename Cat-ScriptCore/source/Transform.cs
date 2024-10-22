a88024a497e6025d924dc281ba892c9e95b833c0
William <williamenglish1220@gmail.com>
2024-10-22 09:05:55 -0400

    Added Transform.cs

---
+++ Cat-ScriptCore/source/Transform.cs
@@ -0,0 +1,19 @@
+﻿namespace CatEngine
+{
+	public struct Transform
+	{
+		public static Vector3 Position
+		{
+			get
+			{
+				MeownoBehaviour.Entity_GetPosition(MeownoBehaviour.GetID(), out Vector3 position);
+				return position;
+			}
+			set
+			{
+				MeownoBehaviour.Entity_SetPosition(MeownoBehaviour.GetID(), ref value);
+			}
+
+		}
+	}
+}
