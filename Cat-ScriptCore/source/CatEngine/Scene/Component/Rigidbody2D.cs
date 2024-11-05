f39257ea33a5dbc2e81863d0107192b602f28706
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-04 20:35:44 -0500

    Added Rigidbody2D Class

---
+++ Cat-ScriptCore/source/CatEngine/Scene/Component/Rigidbody2D.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CatEngine
+{
+	public class Rigidbody2D : Component
+	{
+		public void ApplyLinearImpulse(Vector2 impulse, Vector2 worldPoint, bool wake)
+		{
+			InternalCalls.Rigidbody2D_ApplyLinearImpulse(Object.ID, ref impulse, ref worldPoint, wake);
+		}
+		public void ApplyLinearImpulse(Vector2 impulse, bool wake)
+		{
+			InternalCalls.Rigidbody2D_ApplyLinearImpulseToCenter(Object.ID, ref impulse, wake);
+		}
+	}
+}
