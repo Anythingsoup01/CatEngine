907c72064023c8acbecd451b452a11f8b2a866d3
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-04 20:33:39 -0500

    Replaced MeownoBehaviour with Object Class

---
+++ Cat-ScriptCore/source/CatEngine/Scene/Component/Object.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatEngine
+{
+	public class Object
+	{
+		protected Object() { ID = 0; }
+
+		internal Object(ulong id)
+		{
+			ID = id;
+		}
+
+		public readonly ulong ID;
+
+		
+	}
+}
