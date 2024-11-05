3aa14b5b11919e7077e5a6ff1e360e293b9411e1
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-04 20:34:32 -0500

    Moved & Updated Component Class

---
+++ Cat-ScriptCore/source/CatEngine/Scene/Component/Component.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CatEngine
+{
+	public abstract class Component : CatEngine.Object
+	{
+
+		public Object Object { get; internal set; }
+
+		public Component gameObject
+		{
+			get
+			{
+				return this;
+			}
+		}
+
+		public string tag
+		{
+			get
+			{
+				return gameObject.tag;
+			}
+			set
+			{
+				gameObject.tag = value;
+			}
+		}
+
+		public Layer layer
+		{
+			get
+			{
+				return gameObject.layer;
+			}
+		}
+
+		public Vector3 position
+		{
+			get
+			{
+				InternalCalls.Transform_GetPosition(ID, out Vector3 result);
+				return result;
+			}
+			set
+			{
+				InternalCalls.Transform_SetPosition(ID, ref value);
+			}
+		}
+
+		public bool HasComponent<T>() where T : Component, new()
+		{
+			Type componentType = typeof(T);
+			return InternalCalls.Entity_HasComponent(ID, componentType);
+		}
+
+		public T GetComponent<T>() where T : Component, new()
+		{
+			if (!HasComponent<T>())
+				return null;
+
+			T component = new T() { Object = this };
+			return component;
+		}
+
+	}
+}
