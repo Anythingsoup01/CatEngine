3675902c6b6ca20d9f4d460f8fc8ae8a9e6cd4d4
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-04 20:36:10 -0500

    Added Rigidbody2D functions

--- Cat-ScriptCore/source/CatEngine/InternalCalls.cs
+++ Cat-ScriptCore/source/CatEngine/InternalCalls.cs
@@ -3,11 +3,14 @@ using System.Runtime.CompilerServices;
 
 namespace CatEngine
 {
-	internal class InternalCalls
+	public static class InternalCalls
 	{
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		internal extern static bool Entity_HasComponent(ulong entityID, Type component);
+		internal extern static bool Entity_HasComponent(ulong entityID, Type componentType);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		internal extern static bool Input_IsKeyDown(KeyCode keyCode);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		internal extern static void Transform_GetPosition(ulong entityID, out Vector3 position);
@@ -16,6 +19,9 @@ namespace CatEngine
 		internal extern static void Transform_SetPosition(ulong entityID, ref Vector3 position);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		internal extern static bool Input_IsKeyDown(KeyCode keyCode);
+		internal extern static void Rigidbody2D_ApplyLinearImpulse(ulong entityID, ref Vector2 impulse, ref Vector2 point, bool wake);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		internal extern static void Rigidbody2D_ApplyLinearImpulseToCenter(ulong entityID, ref Vector2 impulse, bool wake);
 	}
 }
