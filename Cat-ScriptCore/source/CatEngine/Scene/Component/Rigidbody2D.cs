1938a5e96a88e47ad43167a872f66f68dc5c4221
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-06 10:28:26 -0500

    Added ApplyForce Functions

--- Cat-ScriptCore/source/CatEngine/Scene/Component/Rigidbody2D.cs
+++ Cat-ScriptCore/source/CatEngine/Scene/Component/Rigidbody2D.cs
@@ -3,6 +3,14 @@ namespace CatEngine
 {
 	public class Rigidbody2D : Component
 	{
+		public void ApplyForce(Vector2 impulse, Vector2 worldPoint, bool wake)
+		{
+			InternalCalls.Rigidbody2D_ApplyForce(Object.ID, ref impulse, ref worldPoint, wake);
+		}
+		public void ApplyForce(Vector2 impulse, bool wake)
+		{
+			InternalCalls.Rigidbody2D_ApplyForceToCenter(Object.ID, ref impulse, wake);
+		}
 		public void ApplyLinearImpulse(Vector2 impulse, Vector2 worldPoint, bool wake)
 		{
 			InternalCalls.Rigidbody2D_ApplyLinearImpulse(Object.ID, ref impulse, ref worldPoint, wake);
