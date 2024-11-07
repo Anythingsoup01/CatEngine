bcc9381e4193486decda04c3ee3e44bcedeb0833
AnythingSoup01 <williamenglish1220@gmail.com>
2024-11-06 22:20:12 -0500

    Moved to SampleProject Folder

---
+++ CatEditor/SampleProject/Assets/Scripts/Source/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CatEngine;
+
+public class FlyingCameraController : MeownoBehaviour
+{
+	public float m_MoveSpeed = 1f;
+	public float m_RotateSpeed = 1f;
+
+	public Vector3 m_Velocity;
+
+	private Transform m_Transform;
+
+	void Start()
+	{
+		m_Transform = GetComponent<Transform>();
+	}
+
+	private void Update(float ts)
+	{
+
+
+	}
+
+}
+
---
+++ CatEditor/SampleProject/Assets/Scripts/Source/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using CatEngine;
+
+public class PlayerController : MeownoBehaviour
+{
+
+	public float m_Speed = 0f;
+	public Vector2 m_Velocity;
+
+	private float m_Vertical;
+	private float m_Horizontal;
+
+//	public CameraController m_CameraController;
+
+	private Transform m_Transform;
+	private Rigidbody2D m_Rigidbody2D;
+
+	private void Start()
+	{
+		m_Transform = GetComponent<Transform>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+		Console.WriteLine("Player Controller - Start");
+	}
+
+	private void Update(float ts)
+	{
+
+
+		m_Velocity = Vector2.zero;
+
+		Vector2 vertical;
+		Vector2 horizontal;
+
+
+
+		if (Input.IsKeyDown(KeyCode.W))
+			vertical.X = 1f;
+		else
+			vertical.X = 0f;
+		if (Input.IsKeyDown(KeyCode.S))
+			vertical.Y = -1f;
+		else
+			vertical.Y = 0f;
+		if (Input.IsKeyDown(KeyCode.D))
+			horizontal.X = 1f;
+		else
+			horizontal.X = 0f;
+		if (Input.IsKeyDown(KeyCode.A))
+			horizontal.Y = -1f;
+		else
+			horizontal.Y = 0f;
+
+		if (Input.IsKeyDown(KeyCode.R))
+			m_Transform.Position = Vector3.zero;
+
+		m_Vertical = vertical.X + vertical.Y;
+		m_Horizontal = horizontal.X + horizontal.Y;
+
+
+		m_Velocity = new Vector2(m_Horizontal, m_Vertical).normalized * m_Speed * ts;
+
+		m_Rigidbody2D.ApplyLinearImpulse(m_Velocity, true);
+
+
+	}
+
+
+}
+
